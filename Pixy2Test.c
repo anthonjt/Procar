24e4a14be6be9b54f5195e97c4c6f3efe8c8f295
anthonjt <46004587+anthonjt@users.noreply.github.com>
2019-10-11 00:20:55 -0400

    Update Pixy2Test.c

--- Pixy2Test.c
+++ Pixy2Test.c
@@ -3,28 +3,11 @@
 #include "fsl_dspi.h"
 #include "board.h"
 #include "Pixy2.h"
-
 #include "pin_mux.h"
 #include "clock_config.h"
 #include "fsl_uart.h"
-/*******************************************************************************
- * Definitions
- ******************************************************************************/
-#define EXAMPLE_DSPI_MASTER_BASEADDR SPI0
-#define DSPI_MASTER_CLK_SRC DSPI0_CLK_SRC
-#define DSPI_MASTER_CLK_FREQ CLOCK_GetFreq(DSPI0_CLK_SRC)
-#define EXAMPLE_DSPI_MASTER_PCS_FOR_INIT kDSPI_Pcs0
-#define EXAMPLE_DSPI_MASTER_PCS_FOR_TRANSFER kDSPI_MasterPcs0
 
-#define EXAMPLE_DSPI_DEALY_COUNT 0XFFFFFU
-#define TRANSFER_SIZE 64U         /*! Transfer dataSize */
-#define TRANSFER_BAUDRATE 2000000U /*! Transfer baudrate - 2 megahertz */
-
-/*!
- * @brief Main function
- */
-int main(void)
-{
+int main(void){
     BOARD_InitPins();
     BOARD_BootClockRUN();
     BOARD_InitDebugConsole();
@@ -32,23 +15,23 @@ int main(void)
     while (1){
         PRINTF("\r\n Press any key to run\r\n");
         GETCHAR();
-        
+
         //getBlocks test
 		uint8_t* getBlocksData = getBlocks();
         PRINTF("getBlocks test");
 		for (int i=0; i<sizeof(getBlocksData); i++){
 			PRINTF(" %02X", getBlocksData[i]);
 		}
-        
+
         //setServos test
         uint16_t servo1 = 237;
-        uint16_t servo2 = 417
+        uint16_t servo2 = 417;
         uint8_t* setServosData = setServos(servo1, servo2);
         PRINTF("setServos test");
         for (int i=0; i<sizeof(setServosData); i++){
             PRINTF(" %02X", setServosData[i]);
         }
-        
+
         //setLED test
         uint8_t red = 200;
         uint8_t green = 100;
@@ -58,29 +41,29 @@ int main(void)
         for (int i=0; i<sizeof(setLEDData); i++){
             PRINTF(" %02X", setLEDData[i]);
         }
-        
+
         //setCameraBrightness test
-        uint8_t brightness = 130;
+        uint8_t brightness = 138;
         uint8_t* setCameraBrightnessData = setCameraBrightness(brightness);
         PRINTF("setCameraBrightness test");
         for (int i=0; i<sizeof(setCameraBrightnessData); i++){
             PRINTF(" %02X", setCameraBrightnessData[i]);
         }
-        
+
         //getResolution test
         uint8_t* getResolutionData = getResolution();
         PRINTF("getResolution test");
         for (int i=0; i<sizeof(getResolutionData); i++){
             PRINTF(" %02X", getResolutionData[i]);
         }
-        
+
         //getVersion test
         uint8_t* getVersionData = getVersion();
         PRINTF("getVersion test");
-        for (int i=0; i<sizeof(getVerisoData); i++){
-            PRINTF(" %02X", getVerisonData[i]);
+        for (int i=0; i<sizeof(getVersionData); i++){
+            PRINTF(" %02X", getVersionData[i]);
         }
-        
+
         //setLamp test
         uint8_t upperlight = 1;
         uint8_t lowerlight = 1;
@@ -89,21 +72,21 @@ int main(void)
         for (int i=0; i<sizeof(setLampData); i++){
             PRINTF(" %02X", setLampData[i]);
         }
-        
+
         //getFPS test
         uint8_t* getFPSData = getFPS();
         PRINTF("getFPS test");
         for (int i=0; i<sizeof(getFPSData); i++){
             PRINTF(" %02X", getFPSData[i]);
         }
-        
+
         //getMainFeatures test
         uint8_t* getMainFeaturesData = getMainFeatures();
         PRINTF("getMainFeatures test");
         for (int i=0; i<sizeof(getMainFeaturesData); i++){
-            PRINTF(" %02X", getMainFeaaturesData[i]);
+            PRINTF(" %02X", getMainFeaturesData[i]);
         }
-        
+
         //setMode test
         uint8_t mode = 2;
         uint8_t* setModeData = setMode(mode);
@@ -111,7 +94,7 @@ int main(void)
         for (int i=0; i<sizeof(setModeData); i++){
             PRINTF(" %02X", setModeData[i]);
         }
-        
+
         //setNextTurn test
         uint8_t nextTurnAngle = 175;
         uint8_t* setNextTurnData = setNextTurn(nextTurnAngle);
@@ -119,7 +102,7 @@ int main(void)
         for (int i=0; i<sizeof(setNextTurnData); i++){
             PRINTF(" %02X", setNextTurnData[i]);
         }
-        
+
         //setDefaultTurn test
         uint8_t defaultTurnAngle = 130;
         uint8_t* setDefaultTurnData = setDefaultTurn(defaultTurnAngle);
@@ -127,7 +110,7 @@ int main(void)
         for (int i=0; i<sizeof(setDefaultTurnData); i++){
             PRINTF(" %02X", setDefaultTurnData[i]);
         }
-        
+
         //setVector test
         uint8_t setVectorAngle = 130;
         uint8_t* setVectorData = setVector(setVectorAngle);
@@ -135,18 +118,21 @@ int main(void)
         for (int i=0; i<sizeof(setVectorData); i++){
             PRINTF(" %02X", setVectorData[i]);
         }
-        
+
         //reverseVector test
         uint8_t* reverseVectorData = reverseVector();
         PRINTF("reverseVector test");
         for (int i=0; i<sizeof(reverseVectorData); i++){
             PRINTF(" %02X", reverseVectorData[i]);
         }
-        
+
         //getRGB test
-        uint8_t* getRGBData = getRGB();
+        uint16_t x = 175;
+        uint16_t y = 115;
+        uint8_t saturate = 1;
+        uint8_t* getRGBData = getRGB(x, y, saturate);
         PRINTF("getRGB test");
-        for (int i=0; i<sizeof(getRGBDatta); i++){
+        for (int i=0; i<sizeof(getRGBData); i++){
             PRINTF(" %02X", getRGBData[i]);
         }
     }
