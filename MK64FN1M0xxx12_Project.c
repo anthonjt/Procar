df6c9a502cdad0ecb7618f30608c7c657a32c145
anthonjt <46004587+anthonjt@users.noreply.github.com>
2019-11-07 14:05:34 -0500

    Update MK64FN1M0xxx12_Project.c

--- MK64FN1M0xxx12_Project.c
+++ MK64FN1M0xxx12_Project.c
@@ -1,10 +1,14 @@
+#include <stdio.h>
+#include "board.h"
+#include "peripherals.h"
+#include "pin_mux.h"
 #include "clock_config.h"
 #include "MK64F12.h"
 #include "fsl_debug_console.h"
 
+
 void PWM_init(void);
-void ADC0_init(void);
-void DC_Motor(void);
+//void ADC0_init(void);
 
 #define mod 2047
 
@@ -17,8 +21,6 @@ int main(void) {
     BOARD_InitDebugConsole();
     __disable_irq();        /* global disable IRQs */
     PWM_init();
-    FTM2_IRQHandler();
-    __enable_irq();
 
 	while(1) {
 
@@ -27,39 +29,42 @@ int main(void) {
 
 void PWM_init(void) // Also enables the TOF interrupt
 	{
-	SIM->SCGC5 |= SIM_SCGC5_PORTB_MASK;       // enable clock to Port C*/
+	SIM->SCGC5 |= SIM_SCGC5_PORTA_MASK;       // enable clock to Port C*/
 	//SIM->SCGC5 |= 0x2000;		// enable clock to Port E
-	SIM->SCGC6 |= SIM_SCGC6_FTM2_MASK;   // enable clock to TPM0,1,2 */
-	PORTB->PCR[18] = PORT_PCR_MUX(3);     // PTC2 used by TPM0 */
-	PTB->PDDR |= 0x40000;
+	SIM->SCGC6 |= SIM_SCGC6_FTM0_MASK;   // enable clock to TPM0,1,2 */
+	PORTA->PCR[1] = PORT_PCR_MUX(3);     // PTC2 used by TPM0 */
+	PTA->PDDR |= 0x1;
 	//	PORTE->PCR[21] = 0x300;
 //	PORTE->PCR[23] = 0x300;
 //	SIM->SOPT2 |= 0x01000000;   // use MCGFLLCLK as timer counter clock */
-	NVIC_EnableIRQ(FTM2_IRQn);		/*Enable the FTM Interrupt*/
-	FTM2->SC = 0;               // disable timer */
-	FTM2->CONTROLS[1].CnSC = 0x20|0x08;   /* edge-aligned, pulse high MSB:MSA=10, ELSB:ELSA=10*/
-	FTM2->MOD = mod;            // Set up modulo register for 42.6 us period or 23.3 kHz rate */
-	FTM2->CONTROLS[1].CnV = mod/2;  /* Set up default channel value for 50% dutycycle */
-	FTM2->SC |= 0x80;           // clear TOF */
-	FTM2->SC |= 0x40;           // enable timer with prescaler /8 For DC motor and Buzzer*/
-	FTM2->SC |= 0x0B;
-	NVIC->ISER[0] |= 0x00080000;
+	NVIC_EnableIRQ(FTM0_IRQn);		/*Enable the FTM Interrupt*/
+	FTM0->SC = 0;               // disable timer */
+	FTM0->CONTROLS[6].CnSC = FTM_CnSC_MSB_MASK|FTM_CnSC_ELSB_MASK;
+	FTM0->MOD = mod;            // Set up modulo register for 42.6 us period or 23.3 kHz rate */
+	FTM0->CONF = FTM_CONF_BDMMODE(3);
+	FTM0->SC |= 0x80;           // clear TOF */
+	FTM0->SC |= 0x40;           // enable timer with prescaler /8 For DC motor and Buzzer*/
+	FTM0->SC |= 0x0B;
+
+	FTM0->CONTROLS[6].CnV = mod/2;  /* Set up default channel value for 50% dutycycle */
+	//NVIC->ISER[0] |= 0x00080000;
 }
 
-void FTM2_IRQHandler(void){
-	NVIC->ISER[0] &= ~0x00080000;
+void FTM0_IRQHandler(void){
+	//NVIC->ISER[0] &= ~0x00080000;
+	FTM0->SC |= 0x0080;
 	int speed = 50;
-	FTM2->MOD = mod/2;
-	FTM2->CONTROLS[1].CnV = (mod*speed)/100;  // Set up channel value 50% Duty cycle
+	FTM0->MOD = mod/2;
+	FTM0->CONTROLS[6].CnV = (mod*speed)/100;  // Set up channel value 50% Duty cycle
 }
 
-void ADC0_init(void)
-	{
-	SIM->SCGC5 |= 0x2000;       /* clock to PORTE */
-	PORTE->PCR[20] = 0;         /* PTE20 analog input */
-	PORTE->PCR[22] = 0;
-	SIM->SCGC6 |= 0x8000000;    /* clock to ADC0 */
-	ADC0->SC2 &= ~0x40;         /* software trigger */
-	ADC0->SC3 = 0x8;			/* 4 sample average */
-	ADC0->CFG1 = 0x20 	;		 /* clock div by 4, long sample time, single ended 12 bit, bus clock */
-}
+//void ADC0_init(void)
+//	{
+//	SIM->SCGC5 |= 0x2000;       /* clock to PORTE */
+//	PORTE->PCR[20] = 0;         /* PTE20 analog input */
+//	PORTE->PCR[22] = 0;
+//	SIM->SCGC6 |= 0x8000000;    /* clock to ADC0 */
+//	ADC0->SC2 &= ~0x40;         /* software trigger */
+//	ADC0->SC3 = 0x8;			/* 4 sample average */
+//	ADC0->CFG1 = 0x20 	;		 /* clock div by 4, long sample time, single ended 12 bit, bus clock */
+//}
