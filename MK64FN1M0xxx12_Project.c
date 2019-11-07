763e355f535c9b86e0ee8847984e40df316a0c3f
Caleb Farleigh <42849802+farleicj@users.noreply.github.com>
2019-11-07 11:34:45 -0500

    Add files via upload

--- MK64FN1M0xxx12_Project.c
+++ MK64FN1M0xxx12_Project.c
@@ -19,10 +19,14 @@ int speed = 50;
 #define mod 2047
 
 int main(void) {
-	__disable_irq();        // global disable IRQs /1
-	UART0_init();
-	PWM_init();                     // Configure PWM /
-	__enable_irq();         // global enable IRQs /
+	/* Init board hardware. */
+    BOARD_InitBootPins();
+    BOARD_InitBootClocks();
+    BOARD_InitBootPeripherals();
+  	/* Init FSL debug console. */
+    BOARD_InitDebugConsole();
+    __disable_irq();        /* global disable IRQs */
+
 	while(1) {
 
 	}
@@ -30,25 +34,26 @@ int main(void) {
 
 void PWM_init(void) // Also enables the TOF interrupt
 	{
-	SIM->SCGC5 |= 0x0800;       // enable clock to Port C*/
-	SIM->SCGC5 |= 0x2000;		// enable clock to Port E
-	SIM->SCGC6 |= 0x07000000;   // enable clock to TPM0,1,2 */
+	SIM->SCGC5 |= SIM_SCGC5_PORTC_MASK;       // enable clock to Port C*/
+	//SIM->SCGC5 |= 0x2000;		// enable clock to Port E
+	SIM->SCGC6 |= SIM_SCGC6_FTM2_MASK;   // enable clock to TPM0,1,2 */
 	PORTC->PCR[2] = 0x0400;     // PTC2 used by TPM0 */
-	PORTE->PCR[21] = 0x300;
-	PORTE->PCR[23] = 0x300;
-	SIM->SOPT2 |= 0x01000000;   // use MCGFLLCLK as timer counter clock */
-	FTM3->SC = 0;               // disable timer */
-	FTM3->CONTROLS[1].CnSC = 0x20|0x08;   /* edge-aligned, pulse high MSB:MSA=10, ELSB:ELSA=10*/
-	FTM3->MOD = mod;            // Set up modulo register for 42.6 us period or 23.3 kHz rate */
-	FTM3->CONTROLS[1].CnV = mod/10;  /* Set up default channel value for 50% dutycycle */
-	FTM3->SC |= 0x80;           // clear TOF */
-	FTM3->SC |= 0x0B;           // enable timer with prescaler /8 For DC motor and Buzzer*/
+//	PORTE->PCR[21] = 0x300;
+//	PORTE->PCR[23] = 0x300;
+//	SIM->SOPT2 |= 0x01000000;   // use MCGFLLCLK as timer counter clock */
+	 NVIC_EnableIRQ(FTM3_IRQn);		/*Enable the FTM Interrupt*/
+	FTM2->SC = 0;               // disable timer */
+	FTM2->CONTROLS[1].CnSC = 0x20|0x08;   /* edge-aligned, pulse high MSB:MSA=10, ELSB:ELSA=10*/
+	FTM2->MOD = mod;            // Set up modulo register for 42.6 us period or 23.3 kHz rate */
+	FTM2->CONTROLS[1].CnV = mod/10;  /* Set up default channel value for 50% dutycycle */
+	FTM2->SC |= 0x80;           // clear TOF */
+	FTM2->SC |= 0x0B;           // enable timer with prescaler /8 For DC motor and Buzzer*/
 	NVIC->ISER[0] |= 0x00080000;
 }
 
 void DC_Motor(void){
-	FTM3->MOD = mod/2;
-	FTM3->CONTROLS[1].CnV = (mod*speed)/100;  // Set up channel value 50% Duty cycle
+	FTM2->MOD = mod/2;
+	FTM2->CONTROLS[1].CnV = (mod*speed)/100;  // Set up channel value 50% Duty cycle
 }
 
 void ADC0_init(void)
