9c14d19f845d447dea19179ddebba555b3a55031
anthonjt <46004587+anthonjt@users.noreply.github.com>
2019-11-07 12:13:21 -0500

    Update MK64FN1M0xxx12_Project.c

--- MK64FN1M0xxx12_Project.c
+++ MK64FN1M0xxx12_Project.c
@@ -1,11 +1,3 @@
-/**
- * @file    MK64FN1M0xxx12_Project.c
- * @brief   Application entry point.
- */
-#include <stdio.h>
-#include "board.h"
-#include "peripherals.h"
-#include "pin_mux.h"
 #include "clock_config.h"
 #include "MK64F12.h"
 #include "fsl_debug_console.h"
@@ -26,7 +18,8 @@ int main(void) {
   	/* Init FSL debug console. */
     BOARD_InitDebugConsole();
     __disable_irq();        /* global disable IRQs */
-
+    PWM_init();
+    DC_Motor();
 	while(1) {
 
 	}
@@ -34,14 +27,15 @@ int main(void) {
 
 void PWM_init(void) // Also enables the TOF interrupt
 	{
-	SIM->SCGC5 |= SIM_SCGC5_PORTC_MASK;       // enable clock to Port C*/
+	SIM->SCGC5 |= SIM_SCGC5_PORTB_MASK;       // enable clock to Port C*/
 	//SIM->SCGC5 |= 0x2000;		// enable clock to Port E
 	SIM->SCGC6 |= SIM_SCGC6_FTM2_MASK;   // enable clock to TPM0,1,2 */
-	PORTC->PCR[2] = 0x0400;     // PTC2 used by TPM0 */
-//	PORTE->PCR[21] = 0x300;
+	PORTB->PCR[18] = PORT_PCR_MUX(3);     // PTC2 used by TPM0 */
+	PTB->PDDR |= 0x40000;
+	//	PORTE->PCR[21] = 0x300;
 //	PORTE->PCR[23] = 0x300;
 //	SIM->SOPT2 |= 0x01000000;   // use MCGFLLCLK as timer counter clock */
-	 NVIC_EnableIRQ(FTM3_IRQn);		/*Enable the FTM Interrupt*/
+	NVIC_EnableIRQ(FTM2_IRQn);		/*Enable the FTM Interrupt*/
 	FTM2->SC = 0;               // disable timer */
 	FTM2->CONTROLS[1].CnSC = 0x20|0x08;   /* edge-aligned, pulse high MSB:MSA=10, ELSB:ELSA=10*/
 	FTM2->MOD = mod;            // Set up modulo register for 42.6 us period or 23.3 kHz rate */
@@ -52,6 +46,8 @@ void PWM_init(void) // Also enables the TOF interrupt
 }
 
 void DC_Motor(void){
+	NVIC->ISER[0] &= ~0x00080000;
+
 	FTM2->MOD = mod/2;
 	FTM2->CONTROLS[1].CnV = (mod*speed)/100;  // Set up channel value 50% Duty cycle
 }
