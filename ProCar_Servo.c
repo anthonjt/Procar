0a555f205166971d031a0e8e4695273fe7fc3bd5
Robert Tung <tungr@clarkson.edu>
2019-11-07 20:51:08 -0500

    Code for working Servo
    Turns with left, right, and center

--- ProCar_Servo.c
+++ ProCar_Servo.c
@@ -1,3 +1,5 @@
+/* PWM on PTC2 */
+
 #include <stdio.h>
 #include "board.h"
 #include "peripherals.h"
@@ -6,56 +8,51 @@
 #include "MK64F12.h"
 #include "fsl_debug_console.h"
 
-#define mod 2047 /* CHECK FOR CORRECTNESS */
+#define FTM0_MOD_VALUE 37500 // Value closest to 65535 without going over to reach 2ms (50Hz) delay (60MHz/2^PS X 50)
+#define FTM0_CV_VALUE 18750 // 37500/18750 = 2
+#define FTM0_CLK_PRESCALE 5 // Power used to reach MOD value (2^5). Lower = better resolution
+#define FTM0_CH1_CLK_SOURCE 1
 
-void FTM0_init(void);
 
-int main(void) {
-  	/* Init board hardware. */
-    BOARD_InitBootPins();
+int main(void)
+{
+
+    /* Init board hardware. */
     BOARD_InitBootClocks();
     BOARD_InitBootPeripherals();
-  	/* Init FSL debug console. */
+    /* Init FSL debug console. */
     BOARD_InitDebugConsole();
-    __disable_irq();        /* global disable IRQs */
 
-    FTM0_init();
+    SIM->SCGC5 |= SIM_SCGC5_PORTC_MASK;    /*Enable the PORTC Clock*/
+    SIM->SCGC6 |= SIM_SCGC6_FTM0_MASK;    /*Enable the FTM0 Clock*/
+    PORTC->PCR[2] = PORT_PCR_MUX(4);        /*MUX = ALT 4*/
+    BITBAND_REG(GPIOC->PDDR, 2) = 1;     //Make PTD pin 1 output
 
-     int i;
-     for (;;) {
-         i++;						/*Just a Count*/
+
+    FTM0->SC = 0;  //Make sure its Off!
+    FTM0->CNTIN = 0;  // starting value for counter
+    FTM0->CNT= 0; // loads the counter with CNTIN
+    // Alignment must be set via this command for all used channels and
+    // before the pulse width command may be altered from FTM3->CONTROLS[1].CnV
+    FTM0->CONTROLS[1].CnSC = FTM_CnSC_MSB_MASK | FTM_CnSC_ELSB_MASK; //FTM0_CH1
+    FTM0->MOD = FTM0_MOD_VALUE; // set the mod value
+    //  Optionally, if you want FTM to operate during debug, set the BDM mode
+    FTM0->CONF = FTM_CONF_BDMMODE(3); // set FTM to operate normally in BDM mode
+    FTM0->SC  |= FTM_SC_PS(FTM0_CLK_PRESCALE); //divide the input clock down by 2^FTM3_CLK_PRESCALE
+    FTM0->SC  |= FTM_SC_CLKS(FTM0_CH1_CLK_SOURCE);
+    FTM0->SC  |= FTM_SC_TOIE(1);
+    NVIC_EnableIRQ(FTM0_IRQn);            /*Enable the FTM Interrupt*/
+    FTM0->CONTROLS[1].CnV = FTM0_MOD_VALUE*0.075; // Use 0.05, 0.075, 0.1
+
+
+     while (1){
      }
      return 0;
- }
-void FTM0_init(void)
-{
-    SIM->SCGC5 |= SIM_SCGC5_PORTC_MASK;	/*Enable the PORTC Clock*/
-    SIM->SCGC6 |= SIM_SCGC6_FTM0_MASK;	/*Enable the FTM0 Clock*/
-    PORTC->PCR[2] = PORT_PCR_MUX(4);		/*MUX = ALT 4*/
-    PTC->PDDR |= 0x04;                 /*Make PTC2 as output pin*/
-
-    NVIC_EnableIRQ(FTM0_IRQn);		/*Enable the FTM Interrupt*/
-    FTM0->SC = 0;				/*Setting TOIE = 1,CLKS =  01, PS = 111*/
-    FTM0->MOD = mod;				/*Setting the Modulo register = 32000*/
-    FTM0->CONTROLS[1].CnSC = FTM_CnSC_MSB_MASK|FTM_CnSC_ELSB_MASK;				/*Setting MSB = 1, ELSnB = 1 //0x0028  */
-    FTM0->CONTROLS[1].CnV = mod/2;					/*Value of the Channel (32000)*/
-    FTM0->SC |= 0x80;
-    FTM0->SC |= 0x40;
-	FTM0->SC |= 0x0C;
-    FTM0->CONF = FTM_CONF_BDMMODE(3);
 }
- void FTM0_IRQHandler (void)
- {
+
+void FTM0_IRQHandler (void)
+{
    //Clear interrupt flag
- 	unsigned long ChannelValue = FTM0->CONTROLS[1].CnV;			/*Take the value of the Channel to compare it*/
    (void)FTM0->SC;
-   FTM0->SC |= 0x0080;								/*FTM counter has overflow*/
-   if(ChannelValue < 32000)							/*Channel Value > Modulo Value*/
-   {
- 	  FTM0->CONTROLS[1].CnV += 500;								/*Add 500 to Channel*/
-   }
-   else
-   {
- 	  FTM0->CONTROLS[1].CnV = 0;									/*Set Channel in 0*/
-   }
- }
+   FTM0->SC |= 0x0080;                                /*FTM counter has overflow*/
+}
