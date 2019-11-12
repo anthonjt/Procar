1f1371078af49126c073535325d583a193c9f88e
anthonjt <46004587+anthonjt@users.noreply.github.com>
2019-11-12 13:28:59 -0500

    Update DC_Motor.h

--- DC_Motor.h
+++ DC_Motor.h
@@ -1,12 +1,11 @@
 #ifndef DC_Motor_H
 #define DC_Motor_H
 
-#define speed 50
-
-#define mod 2047
+#define FTM3_MOD_VALUE 37500 // Value closest to 65535 without going over to reach 2ms (50Hz) delay (60MHz/2^PS X 50)
+#define FTM3_CLK_PRESCALE 0 // Power used to reach MOD value (2^5). Lower = better resolution
+#define FTM3_CH6_CLK_SOURCE 1
 
 void PWM_init_1(void);
-void ADC1_init(void);
-void DC_Motor(void);
+void FTM3_IRQHandler(void);
 
 #endif
