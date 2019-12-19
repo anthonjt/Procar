3b1acd2898625fe19b78788e81c7c27ca9924828
Caleb Farleigh <42849802+farleicj@users.noreply.github.com>
2019-12-19 11:16:03 -0500

    Update DC_Motor.c

--- DC_Motor.c
+++ DC_Motor.c
@@ -10,15 +10,15 @@ void PWM_initfun(void){
     SIM->SCGC5 |= SIM_SCGC5_PORTC_MASK;  /*Enable the PORTC Clock*/
     SIM->SCGC3 |= SIM_SCGC3_FTM3_MASK;   /*Enable the FTM0 Clock*/
     PORTC->PCR[10] = PORT_PCR_MUX(3);     /*MUX = ALT 3*/
-    BITBAND_REG(GPIOC->PDDR, 10) = 1;     //Make PTC pin 2 output
+    BITBAND_REG(GPIOC->PDDR, 10) = 1;     //Make PTC pin 10 output
     FTM3->SC = 0;  						// Make sure its Off!
     FTM3->CNTIN = 0;  				    // starting value for counter
     FTM3->CNT= 0; 						// loads the counter with CNTIN
-    FTM3->CONTROLS[6].CnSC = FTM_CnSC_MSB_MASK | FTM_CnSC_ELSB_MASK; // FTM0_CH6
+    FTM3->CONTROLS[6].CnSC = FTM_CnSC_MSB_MASK | FTM_CnSC_ELSB_MASK; // FTM3_CH6
     FTM3->MOD = FTM3_MOD_VALUE; 		// set the mod value
     /* Optionally, if you want FTM to operate during debug, set the BDM mode */
     FTM3->CONF = FTM_CONF_BDMMODE(3); 	// Set FTM to operate normally in BDM mode
-    FTM3->SC  |= FTM_SC_PS(FTM3_CLK_PRESCALE); 		// Divide the input clock down by 2^FTM0_CLK_PRESCALE
+    FTM3->SC  |= FTM_SC_PS(FTM3_CLK_PRESCALE); 		// Divide the input clock down by 2^FTM3_CLK_PRESCALE
     FTM3->SC  |= FTM_SC_CLKS(FTM3_CH6_CLK_SOURCE);  // Use the system clk
     //FTM3->SC  |= FTM_SC_TOIE(1);
     FTM3->CONTROLS[6].CnV = FTM3_MOD_VALUE*0.5;//
